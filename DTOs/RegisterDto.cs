7a1a7c86c13fe09e8993965af714755c5810ec82
BrockMiller9 <9brockmiller@gmail.com>
2023-06-27 09:14:23 -0600

    end of section 7

--- DTOs/RegisterDto.cs
+++ DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace API.DTOs
         public string Username { get; set; } //username
 
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; } //password
     }
 }
